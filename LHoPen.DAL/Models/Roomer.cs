4772fe78dba6e1db07268a3e3543cfb8d136dfe0
Jørgen Vig Jensen <jj@nobly.dk>
2017-08-23 13:31:38 +0200

    DAL

---
+++ LHoPen.DAL/Models/Roomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHoPen.DAL.Models
+{
+    public class Roomer
+    {
+        public int RoomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Deposit { get; set; }
+        public string Identification { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+    }
+}
